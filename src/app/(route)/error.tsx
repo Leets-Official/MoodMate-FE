f81f27eb31e07f8215a12e994ac22de616fa043d
nayoung3669 <skdud3669@gmail.com>
2023-11-10 23:48:17 +0900

    :art: [feat] : error, not-found 페이지 추가

---
+++ src/app/(route)/error.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+const error = () => {
+  return (
+    <div>
+      <div className="w-full h-screen flex items-center justify-center" />
+      {/* 에러 캐릭터 필요! */}
+    </div>
+  )
+}
+
+export default error
