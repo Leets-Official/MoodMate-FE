a495d7d2567bb9025054739dd94d952ba1df2168
One-HyeWon <5876675@naver.com>
2023-11-12 22:26:52 +0900

    :hammer:[refactor]: 버튼 스타일 전역 상태 관리 추가

--- src/app/_components/common/NormalButton.tsx
+++ src/app/_components/common/NormalButton.tsx
@@ -1,11 +1,15 @@
-import React, { FC, MouseEvent } from 'react'
+import { MouseEvent } from 'react'
+import { useRecoilState } from 'recoil'
+import { inputValueState } from '@/_atom/input'
 
 interface NormalButtonProps {
   buttonText: string
-  onClick?: () => void
+  onClick: () => void
 }
 
-const NormalButton: FC<NormalButtonProps> = ({ onClick, buttonText }) => {
+const NormalButton = ({ onClick, buttonText }: NormalButtonProps) => {
+  const inputValue = useRecoilState(inputValueState)
+
   const handleButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
     if (onClick) {
       onClick()
@@ -15,16 +19,14 @@ const NormalButton: FC<NormalButtonProps> = ({ onClick, buttonText }) => {
   return (
     <button
       type="button"
-      className="w-full h-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className={`w-full h-full ${
+        inputValue ? 'bg-blue-500 hover:bg-blue-700' : 'bg-gray-300'
+      } text-white font-bold py-2 px-4 rounded`}
       onClick={handleButtonClick}
     >
       {buttonText}
     </button>
   )
-}
-
-NormalButton.defaultProps = {
-  onClick: () => {},
 }
 
 export default NormalButton
