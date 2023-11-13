76a84d86ea283c1159fc6cbb41e664821813ff1d
One-HyeWon <5876675@naver.com>
2023-11-13 17:24:10 +0900

    :hammer:[refactor]: 버튼 스타일 지정 함수 수정

--- src/app/_components/common/NormalButton.tsx
+++ src/app/_components/common/NormalButton.tsx
@@ -5,24 +5,64 @@ import { inputValueState } from '@/_atom/input'
 interface NormalButtonProps {
   buttonText: string
   onClick: () => void
+  buttonType: 'next' | 'yes' | 'no'
 }
 
-const NormalButton = ({ onClick, buttonText }: NormalButtonProps) => {
-  const inputValue = useRecoilState(inputValueState)
+const getButtonStyles = (
+  buttonType: 'next' | 'yes' | 'no',
+  inputValue: string | null,
+) => {
+  switch (buttonType) {
+    case 'next':
+      return {
+        button: inputValue
+          ? 'bg-orange-500 hover:bg-orange-700 rounded-md p-2'
+          : 'bg-gray-300',
+        span: 'text-white',
+      }
+    case 'yes':
+      return {
+        button: inputValue
+          ? 'bg-orange-500 hover:bg-orange-700 rounded-md p-2'
+          : 'bg-gray-300',
+        span: 'text-white',
+      }
+    case 'no':
+      return {
+        button: inputValue
+          ? 'bg-orange-500 hover:bg-orange-700 rounded-md p-2'
+          : 'bg-gray-300',
+        span: 'text-white',
+      }
+    default:
+      return {
+        button: 'bg-gray-300 rounded-md p-2',
+        span: 'text-white-500',
+      }
+  }
+}
+
+const NormalButton = ({
+  onClick,
+  buttonText,
+  buttonType,
+}: NormalButtonProps) => {
+  const [inputValue] = useRecoilState(inputValueState)
 
   const handleButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
-    if (onClick) {
+    if (onClick && inputValue) {
       onClick()
     }
   }
 
+  const buttonStyles = getButtonStyles(buttonType, inputValue)
+
   return (
     <button
       type="button"
-      className={`w-full h-full ${
-        inputValue ? 'bg-blue-500 hover:bg-blue-700' : 'bg-gray-300'
-      } text-white font-bold py-2 px-4 rounded`}
+      className={`w-full h-full ${buttonStyles.button} ${buttonStyles.span}`}
       onClick={handleButtonClick}
+      disabled={!inputValue}
     >
       {buttonText}
     </button>
