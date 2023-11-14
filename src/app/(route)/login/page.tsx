416ed7e39352645f397dc9eefea9b178e8e874a7
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-14 11:23:28 +0900

    :sparkles: [feat]: 카카오 로그인 초기설정

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,3 +1,19 @@
-export default function LoginPage() {
-  return <div>LoginPage</div>
+'use client'
+
+import NormalButton from '@/_components/common/NormalButton'
+
+export default function Login() {
+  const onLoginhandler = () => {
+    const code = new URL(window.location.href).searchParams.get('code')
+    console.log(code)
+  }
+  return (
+    <a href={process.env.KAKAO_AUTH_URL}>
+      <NormalButton
+        buttonText="카카오로 3초만에 로그인"
+        buttonType="next"
+        onClick={onLoginhandler}
+      />
+    </a>
+  )
 }
