8ef91b4e75ee1949ead79a03d26c747158b561b5
nayoung3669 <skdud3669@gmail.com>
2023-11-16 15:31:57 +0900

    :art: [chat] 채팅 웹소켓 custom hook

--- src/app/_hooks/useWebSocket.ts
+++ src/app/_hooks/useWebSocket.ts
@@ -5,19 +5,18 @@ import SockJS from 'sockjs-client'
 const useWebSocket = () => {
   const [client, setClient] = useState<Client | null>(null)
 
-  // useCallback으로 메모이제이션
   const connect = useCallback((roomId: number) => {
-    const sockJs = new SockJS('url') // backend URL (http)
+    const sockJs = new SockJS('/chat')
     const stompClient = new Client({
       webSocketFactory: () => sockJs, // stomp가 사용할 인스턴스
       onConnect: () => {
-        console.log('Connected')
-        stompClient.subscribe(`/chat/${roomId}`, (message) => {
-          console.log('Received', message)
+        console.log('연결됨!')
+        stompClient.subscribe(`/sub/chat/${roomId}`, (message) => {
+          console.log(`/sub/chat/${roomId}로 subscribe 성공!`, message)
         })
       },
       onDisconnect: () => {
-        console.log('Disconnected')
+        console.log('연결 해제!')
       },
       onStompError: (frame) => {
         console.error('Broker reported error:', frame.headers.message)
@@ -28,14 +27,12 @@ const useWebSocket = () => {
     setClient(stompClient)
   }, [])
 
-  const sendMessage = useCallback(
-    (roomId: number, message: string) => {
-      if (client && client.connected) {
-        client.publish({ destination: `/chat/${roomId}`, body: message })
-      }
-    },
-    [client],
-  )
+  const sendMessage = (roomId: number, message: string) => {
+    console.log(roomId, message)
+    if (client && client.connected) {
+      client.publish({ destination: `/pub/chat/${roomId}`, body: message }) // send로 되는지 확인
+    }
+  }
 
   const disconnect = useCallback(() => {
     if (client && client.connected) {
