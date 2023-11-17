0c9eda12271b349ff8e28d76fa6b8bee0b658f2a
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-18 00:25:32 +0900

    :hammer: [refactor]: 아이콘추가

--- src/app/_components/common/NavBar.tsx
+++ src/app/_components/common/NavBar.tsx
@@ -1,17 +1,21 @@
 'use client'
 
 import Icons from '@/_components/common/Icons'
-import { IconPathTypes } from '@/_types/icon'
+import { bio, chat, deactivation } from '@/_ui/IconsPath'
 
 const NavBar = () => {
   return (
     <div className="transform fixed bottom-0 left-1/2 -translate-x-1/2">
-      <div className="w-[378px] h-[140px] shadow-xl rounded-3xl flex justify-between rotate-180">
-        <div className="ml-10 mb-12 rotate-180">비활성화</div>
-        <div className="rotate-180 translate-y-24 bg-[#DADADA] rounded-full w-[72px] h-[72px] text-center">
-          채팅방
+      <div className="w-[378px] h-[140px] shadow-xl rounded-[88px] flex justify-between rotate-180">
+        <div className="ml-14 mb-12 rotate-180">
+          <Icons name={bio} />
         </div>
-        <div className="rotate-180 mr-10 mb-12">마이페이지</div>
+        <div className="rotate-180 translate-y-24 bg-[#DADADA] rounded-full w-[72px] h-[72px]">
+          <Icons name={chat} className="ml-6 mt-6" />
+        </div>
+        <div className="rotate-180 mr-14 mb-12">
+          <Icons name={deactivation} />
+        </div>
       </div>
     </div>
   )
