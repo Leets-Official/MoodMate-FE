8ba7020a4976c1d7558e83c55c9aa3aad2c7475c
nayoung3669 <skdud3669@gmail.com>
2023-11-17 21:13:26 +0900

    :recycle: [refactor] : 아이콘 , 아이콘 path 수정

--- src/app/_types/icon.ts
+++ src/app/_types/icon.ts
@@ -4,6 +4,12 @@ export interface IconPathTypes {
   path: string
   width: number
   height: number
-  fill?: string
-  options?: SVGProps<SVGPathElement>
+  fill: string
+  options?: IconOptions
+}
+interface IconOptions {
+  stroke: string
+  strokeWidth: number
+  strokeLinecap?: 'round' | 'butt' | 'square' | 'inherit' | undefined
+  strokeLinejoin?: 'round' | 'bevel' | 'miter' | 'inherit' | undefined
 }
