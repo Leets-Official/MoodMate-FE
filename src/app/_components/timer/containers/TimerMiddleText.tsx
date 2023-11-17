e6bfdca7efaccc0822d1d0beea4101e50fe46df8
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-17 13:54:11 +0900

    [refactor]: 컨스턴트 추가 및 코드 수정

--- src/app/_components/timer/containers/TimerMiddleText.tsx
+++ src/app/_components/timer/containers/TimerMiddleText.tsx
@@ -1,12 +1,10 @@
-import { TIMER_GUIDE } from '@/_constants/main'
+import { BEFORE_TIMER_GUIDE } from '@/_constants/main'
 import React from 'react'
 
 const TimerMiddleText = () => {
   return (
-    <section className="text-center mx-auto">
-      <p className="mt-5 bg-[#313131] px-10 py-5 text-white rounded-[30px]">
-        운명의 무디를 만날 시간!
-      </p>
+    <section className="text-center mx-auto mt-5 bg-[#313131] px-10 py-5 text-white rounded-[30px]">
+      <p>{BEFORE_TIMER_GUIDE.DESCRIPTION}</p>
     </section>
   )
 }
