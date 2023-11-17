9dfe724bb7053c0ffa172d1a2b7c5c9deb7c4961
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-17 23:06:35 +0900

    :sparkles: [feat]: 마이페이지 생성

---
+++ src/app/_components/mypage/containers/MypageThirdBox.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Icons from '@/_components/common/Icons'
+
+const MypageThirdBox = () => {
+  return (
+    <section className="ml-4 mt-2">
+      <p className="text-[12px] text-[#808080]">로그인</p>
+      <div className="mt-3 flex">
+        <p>i</p>
+        <p className="text-[16px] ml-4">로그아웃</p>
+      </div>
+    </section>
+  )
+}
+
+export default MypageThirdBox
