d60871e1f1b15688727dcfff93fd149b3f8e96d8
nayoung3669 <skdud3669@gmail.com>
2023-11-17 21:13:09 +0900

    :recycle: [refactor] : header 컴포넌트 조건부 렌더링 수정

--- src/app/_components/layout/Header.tsx
+++ src/app/_components/layout/Header.tsx
@@ -1,24 +1,16 @@
 'use client'
 
-import { back, home } from '@/_ui/IconsPath'
+import { home } from '@/_ui/IconsPath'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import Icons from '../common/Icons'
 
 const Header = ({ chat }: { chat?: boolean }) => {
-  const router = useRouter()
-
   return (
     <section className="flex items-center justify-between w-full h-[47px] px-[26px]">
-      {chat ? (
-        <h1 className="text-[20px] font-bold">채팅</h1>
-      ) : (
-        <Icons name={back} onClick={() => router.back()} />
-      )}
+      <h1 className="text-[20px] font-bold">채팅</h1>
       <div className="flex flex-row items-center justify-center gap-3">
-        <Link href="/main">
-          <Icons name={home} />
-        </Link>
+        <Icons name={home} />
       </div>
     </section>
   )
