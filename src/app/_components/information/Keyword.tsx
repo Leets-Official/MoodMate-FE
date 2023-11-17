6c0714e886dc7426cf6e50df382828cbdfccc77e
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-18 01:13:57 +0900

    :wrench: [refactor]: 마이페이지 컴포넌트 생성 중

---
+++ src/app/_components/information/Keyword.tsx
@@ -0,0 +1,17 @@
+const Keyword = () => {
+  return (
+    <div className="flex text-[14px] ml-3 my-4">
+      <p className="px-5 py-1.5 rounded-[18px] mr-2 bg-[#666666] text-white">
+        키워드1
+      </p>
+      <p className="px-5 py-1.5 rounded-[18px] mr-2 bg-[#666666] text-white">
+        키워드2
+      </p>
+      <p className="px-5 py-1.5 rounded-[18px] bg-[#666666] text-white">
+        키워드3
+      </p>
+    </div>
+  )
+}
+
+export default Keyword
