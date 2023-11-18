3ad0775450358a67a171ebaf4c3ec91d6413563a
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-18 12:29:29 +0900

    [refacotr]: 마이페이지 UI 수정

---
+++ src/app/_components/information/Department.tsx
@@ -0,0 +1,11 @@
+const Department = () => {
+  return (
+    <div className="flex text-[14px] ml-3 my-2">
+      <p className="px-5 py-1.5 rounded-[18px] mr-2 bg-[#666666] text-white">
+        다른 학과가 좋아요
+      </p>
+    </div>
+  )
+}
+
+export default Department
--- src/app/_components/information/Keyword.tsx
+++ src/app/_components/information/Keyword.tsx
@@ -1,6 +1,6 @@
 const Keyword = () => {
   return (
-    <div className="flex text-[14px] ml-3 my-4">
+    <div className="flex text-[14px] ml-3 my-2">
       <p className="px-5 py-1.5 rounded-[18px] mr-2 bg-[#666666] text-white">
         키워드1
       </p>
