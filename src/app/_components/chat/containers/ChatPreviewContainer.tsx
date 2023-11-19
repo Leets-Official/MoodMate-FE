9cead5d82738259ca06384fd0dda124df6253560
nayoung3669 <skdud3669@gmail.com>
2023-11-19 15:11:59 +0900

    :sparkles: [feat] : 채팅목록 페이지 UI 및 라우팅

--- src/app/_components/chat/containers/ChatPreviewContainer.tsx
+++ src/app/_components/chat/containers/ChatPreviewContainer.tsx
@@ -1,10 +1,25 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import ChatPreview from '../chatlist/ChatPreview'
 
 const ChatPreviewContainer = () => {
+  const [userId, setUserId] = useState<number>(0) // 초깃값 0?
+  useEffect(() => {
+    const user = localStorage.getItem('userId')
+    user && setUserId(() => Number(user))
+  }, [])
   return (
-    <div>
-      <ChatPreview />
-    </div>
+    <section className="bg-neutral-300 w-full h-full flex justify-center pt-[34px]">
+      <ChatPreview
+        userId={userId}
+        nickname="사랑스러운 무디"
+        count={1}
+        lastMessage="안녕하세요. 저랑 취향이 비슷 저랑 취향이 비슷 저랑 취향이 비슷하시네요. 저도 강아지
+          좋아해요~"
+        isRead={false}
+      />
+    </section>
   )
 }
 
