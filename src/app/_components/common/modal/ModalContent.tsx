0bd513decc8c4909104cc9d77e1a4145a1aa2aa6
nayoung3669 <skdud3669@gmail.com>
2023-11-24 19:23:34 +0900

    :art: [refactor] 모달 열렸을 때 스크롤 안되게 수정

--- src/app/_components/common/modal/ModalContent.tsx
+++ src/app/_components/common/modal/ModalContent.tsx
@@ -16,13 +16,15 @@ const ModalContent = ({
   const { TITLE, SUB_TITLE, CONFIRM, CANCEL } = subject
 
   return (
-    <section className=" w-full h-full">
+    <section className=" w-full h-full flex flex-col justify-center items-center gap-[40px]">
       <div>
-        <h1>{TITLE}</h1>
-        <h5>{SUB_TITLE || ''}</h5>
+        <h1 className="text-[#333] text-[18px] font-bold ">{TITLE}</h1>
+        <h5 className="text-[#999] w-full text-center text-xs">
+          {SUB_TITLE || ''}
+        </h5>
       </div>
-      <div>
-        {/* 나중에 dynamic으로 가져오기? */}
+      <div className="w-[169px] h-[193px] bg-neutral-300">
+        {/* 나중에 dynamic으로 가져오기?, 크기 조정 */}
         {onMyPage ? <p>씩씩한 무디 이미지</p> : <p>울고있는 무디 이미지</p>}
       </div>
       <ModalButtons
