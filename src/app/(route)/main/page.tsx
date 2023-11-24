3f5755a41966863f2a80ea6527dc2d61e7aef1ca
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-24 21:39:55 +0900

    :lipstick: [feat]: 메인페이지 매칭 전후 분리

--- src/app/(route)/main/page.tsx
+++ src/app/(route)/main/page.tsx
@@ -6,15 +6,39 @@ import TimerFirstText from '@/_components/timer/containers/TimerFirstText'
 import TimerMiddleText from '@/_components/timer/containers/TimerMiddleText'
 import NavBar from '@/_components/common/NavBar'
 
-export default function HomePage() {
-  return (
-    <div className="flex flex-col mt-2">
-      <p className="p-10 mx-auto text-center text-[#B3B3B3]">moodmate</p>
-      <TimerFirstText />
-      <Timer targetHour={22} />
-      <TimerMiddleText />
-      <p className="p-9 mx-auto text-center">로고</p>
-      <NavBar />
-    </div>
-  )
+interface MatchProps {
+  type: 'BEFORE' | 'AFTER'
 }
+
+const getBGStyle = (type: string) => {
+  switch (type) {
+    case 'BEFORE':
+      return (
+        <div className="flex flex-col mt-2">
+          <p className="p-10 mx-auto text-center text-[#B3B3B3]">moodmate</p>
+          <TimerFirstText type="BEFORE" />
+          <Timer targetHour={22} />
+          <TimerMiddleText type="BEFORE" />
+          <p className="p-9 mx-auto text-center">로고</p>
+          <NavBar type="BEFORE" />
+        </div>
+      )
+    case 'AFTER':
+      return (
+        <div className="flex flex-col pt-2 bg-[#FFE5E7] h-screen">
+          <p className="p-10 mx-auto text-center text-[#FD8188]">moodmate</p>
+          <TimerFirstText type="AFTER" />
+          <Timer targetHour={20} />
+          <TimerMiddleText type="AFTER" />
+          <p className="p-9 mx-auto text-center">로고</p>
+          <NavBar type="AFTER" />
+        </div>
+      )
+    default:
+      return <p>디폴트</p>
+  }
+}
+const MainPage = ({ type }: MatchProps) => {
+  return <div>{getBGStyle('BEFORE')}</div>
+}
+export default MainPage
--- src/app/_components/timer/containers/TimerMiddleText.tsx
+++ src/app/_components/timer/containers/TimerMiddleText.tsx
@@ -1,20 +1,45 @@
-import { BEFORE_TIMER_GUIDE } from '@/_constants/main'
+import { AFTER_TIMER_GUIDE, BEFORE_TIMER_GUIDE } from '@/_constants/main'
 import React from 'react'
 
-const TimerMiddleText = () => {
-  return (
-    <section className="mt-7 flex justify-center items-center text-center flex-col mx-auto">
-      <p className="pt-6 text-[#333333] font-bold bg-[#FFE5E7] w-[221px] h-[74px] rounded-[30px]">
-        {BEFORE_TIMER_GUIDE.DESCRIPTION}
-      </p>
-      <div
-        className="w-0 h-0
+interface TextProps {
+  type: 'BEFORE' | 'AFTER'
+}
+
+const getTextStyle = (type: string) => {
+  switch (type) {
+    case 'BEFORE':
+      return (
+        <div className="mt-7 flex justify-center items-center text-center flex-col mx-auto">
+          <p className="pt-6 text-[#333333] font-bold bg-[#FFE5E7] w-[221px] h-[74px] rounded-[30px]">
+            {BEFORE_TIMER_GUIDE.DESCRIPTION}
+          </p>
+          <div
+            className="w-0 h-0
   border-l-[10px] border-l-transparent
   border-t-[15px] border-t-[#FFE5E7]
   border-r-[10px] border-r-transparent"
-      />
-    </section>
-  )
+          />
+        </div>
+      )
+    case 'AFTER':
+      return (
+        <div className="mt-7 flex justify-center items-center text-center flex-col mx-auto pt-7 text-[#FFFFFF] font-bold bg-[#FD8188] w-[221px] h-[74px] rounded-[30px]">
+          <p>{AFTER_TIMER_GUIDE.DESCRIPTION_ONE}</p>
+          <p className="mb-3.5">{AFTER_TIMER_GUIDE.DESCRIPTION_TWO}</p>
+          <div
+            className="w-0 h-0
+  border-l-[10px] border-l-transparent
+  border-t-[15px] border-t-[#FD8188]
+  border-r-[10px] border-r-transparent"
+          />
+        </div>
+      )
+    default:
+      return <p>디폴트</p>
+  }
+}
+const TimerMiddleText = ({ type }: TextProps) => {
+  return <section>{getTextStyle(type)}</section>
 }
 
 export default TimerMiddleText
