6d8d95de81857eb1bb44af1236c756ad4d0bfd22
nayoung3669 <skdud3669@gmail.com>
2023-11-24 19:24:56 +0900

    :heavy_plus_sign: [chore] : stompjs 설치 (수정중)

--- src/app/_hooks/useWebSocket.ts
+++ src/app/_hooks/useWebSocket.ts
@@ -1,3 +1,4 @@
+import StompJs from 'stompjs'
 import { Client } from '@stomp/stompjs'
 import { useCallback, useState } from 'react'
 import SockJS from 'sockjs-client'
@@ -7,13 +8,20 @@ const useWebSocket = () => {
 
   const connect = useCallback((roomId: number) => {
     const sockJs = new SockJS('/chat')
+    const stomp = StompJs.over(sockJs)
     const stompClient = new Client({
-      webSocketFactory: () => sockJs, // stomp가 사용할 인스턴스
-      onConnect: () => {
-        console.log('연결됨!')
-        stompClient.subscribe(`/sub/chat/${roomId}`, (message) => {
-          console.log(`/sub/chat/${roomId}로 subscribe 성공!`, message)
-        })
+      webSocketFactory: () => sockJs,
+      onConnect: (frame) => {
+        console.log('연결됨!', frame)
+        stompClient
+          .subscribe(
+            `/sub/chat/${roomId}`,
+            (res) => {
+              console.log(`/sub/chat/${roomId} 메시지 : `, JSON.parse(res.body))
+            },
+            {}, //header token 설정
+          )
+          .unsubscribe()
       },
       onDisconnect: () => {
         console.log('연결 해제!')
@@ -31,6 +39,7 @@ const useWebSocket = () => {
     console.log(message)
     if (client && client.connected) {
       client.publish({
+        //send?
         destination: `/pub/chat/`,
         body: JSON.stringify(message),
       })
