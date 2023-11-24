0b7b95f0cac522ca9c30d9d88daca7c606ef8b39
nayoung3669 <skdud3669@gmail.com>
2023-11-24 19:23:59 +0900

    :sparkles: [feat] 채팅 종료 아이콘 추가

--- src/app/(route)/chat/room/[slug]/page.tsx
+++ src/app/(route)/chat/room/[slug]/page.tsx
@@ -5,7 +5,7 @@ import ChatHeader from '@/_components/layout/ChatHeader'
 export default function ChatRoomPage({ params }: { params: { slug: string } }) {
   return (
     <section className="flex flex-col h-screen">
-      <ChatHeader />
+      <ChatHeader userId={Number(params.slug)} />
       <ChatRoomContainer userId={Number(params.slug)} />
       <ChatInputContainer />
     </section>
