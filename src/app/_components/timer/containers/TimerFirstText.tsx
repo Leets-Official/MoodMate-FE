19c05850dd1f397e37807e47dc8a67923c193460
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-24 21:39:55 +0900

    :lipstick: [feat]: 메인페이지 매칭 전후 분리

--- src/app/_components/timer/containers/TimerFirstText.tsx
+++ src/app/_components/timer/containers/TimerFirstText.tsx
@@ -1,12 +1,12 @@
-import { BEFORE_TIMER_GUIDE } from '@/_constants/main'
+import { TIMER_GUIDE } from '@/_constants/main'
 import React from 'react'
 
 const TimerFirstText = () => {
   return (
     <section className="mx-auto text-center">
-      <p className="text-[12px] text-[#919191]">{BEFORE_TIMER_GUIDE.TITLE}</p>
+      <p className="text-[12px] text-[#919191]">{TIMER_GUIDE.TITLE}</p>
       <p className="mt-3 text-[21px] text-[#5A5A5A] font-bold">
-        {BEFORE_TIMER_GUIDE.SUB_TITLE}
+        {TIMER_GUIDE.SUB_TITLE}
       </p>
     </section>
   )
