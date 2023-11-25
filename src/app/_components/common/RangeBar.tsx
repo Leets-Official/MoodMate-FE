866b60b4e0705077672c1bd7351f7164f23c78dc
One-HyeWon <5876675@naver.com>
2023-11-25 22:08:31 +0900

    :sparkles:[feat]: Range Bar CSS 제외 완료

--- src/app/_components/common/RangeBar.tsx
+++ src/app/_components/common/RangeBar.tsx
@@ -1,11 +1,9 @@
-'use client'
-
 import { useState } from 'react'
 import { Range } from 'react-range'
 
 const STEP = 1
-const MIN = 4
-const MAX = 94
+const MIN = 1994
+const MAX = 2004
 
 type RangeBarProps = {
   type: 'single' | 'range'
@@ -13,11 +11,11 @@ type RangeBarProps = {
 
 const RangeBar = ({ type }: RangeBarProps) => {
   const [values, setValues] = useState<number[]>(
-    type === 'single' ? [MIN] : [MIN, MAX],
+    type === 'single' ? [MIN, MIN] : [MIN, MAX],
   )
 
   return (
-    <div className="w-3/10 mx-auto">
+    <div className="h-screen flex items-center justify-center">
       <Range
         step={STEP}
         min={MIN}
@@ -25,31 +23,37 @@ const RangeBar = ({ type }: RangeBarProps) => {
         values={values}
         onChange={(values) => setValues(values)}
         renderTrack={({ props, children }) => (
-          <div
-            {...props}
-            style={{
-              ...props.style,
-              height: '6px',
-              width: '100%',
-              backgroundColor: '#ccc',
-            }}
-          >
+          <div {...props} className="h-1.5 w-full bg-gray-300 rounded-full">
+            {type === 'range' && (
+              <div
+                className="absolute h-1.5 rounded-full bg-black"
+                style={{
+                  width: `${((values[1] - values[0]) / (MAX - MIN)) * 100}%`,
+                  left: `${((values[0] - MIN) / (MAX - MIN)) * 100}%`,
+                }}
+              />
+            )}
             {children}
           </div>
         )}
         renderThumb={({ props }) => (
           <div
             {...props}
-            style={{
-              ...props.style,
-              height: '20px',
-              width: '20px',
-              backgroundColor: '#333',
-              borderRadius: '50%',
-            }}
-          />
+            className="h-5 w-5 bg-black rounded-full flex items-center justify-center"
+          >
+            {type === 'single' && (
+              <div className="absolute top-[-25px] text-white text-sm font-sans p-1 bg-black">
+                {props['aria-valuenow'].toString().slice(-2)}
+              </div>
+            )}
+          </div>
         )}
       />
+      {type === 'range' && values.length > 1 && (
+        <div className="absolute top-[calc(50%-25px)] left-1/2 transform -translate-x-1/2 text-white text-sm font-sans p-1 bg-black">
+          {values[0].toString().slice(-2)} - {values[1].toString().slice(-2)}
+        </div>
+      )}
     </div>
   )
 }
