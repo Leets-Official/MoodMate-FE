708da8cb3645061dff25146a809930bbab9646e1
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-25 20:31:10 +0900

    :hammer: [refactor]: 중복코드 수정

--- src/app/(route)/main/page.tsx
+++ src/app/(route)/main/page.tsx
@@ -1,44 +1,9 @@
-'use client'
+import MainPage from '@/_components/main/MainPage'
 
-import Timer from '@/_components/common/Timer'
-import React from 'react'
-import TimerFirstText from '@/_components/timer/containers/TimerFirstText'
-import TimerMiddleText from '@/_components/timer/containers/TimerMiddleText'
-import NavBar from '@/_components/common/NavBar'
-
-interface MatchProps {
-  type: 'BEFORE' | 'AFTER'
+export default function MainpagePage() {
+  return (
+    <div>
+      <MainPage type="AFTER" />
+    </div>
+  )
 }
-
-const getBGStyle = (type: string) => {
-  switch (type) {
-    case 'BEFORE':
-      return (
-        <div className="flex flex-col mt-2">
-          <p className="p-10 mx-auto text-center text-[#B3B3B3]">moodmate</p>
-          <TimerFirstText type="BEFORE" />
-          <Timer targetHour={22} />
-          <TimerMiddleText type="BEFORE" />
-          <p className="p-9 mx-auto text-center">로고</p>
-          <NavBar type="BEFORE" />
-        </div>
-      )
-    case 'AFTER':
-      return (
-        <div className="flex flex-col pt-2 bg-[#FFE5E7] h-screen">
-          <p className="p-10 mx-auto text-center text-[#FD8188]">moodmate</p>
-          <TimerFirstText type="AFTER" />
-          <Timer targetHour={20} />
-          <TimerMiddleText type="AFTER" />
-          <p className="p-9 mx-auto text-center">로고</p>
-          <NavBar type="AFTER" />
-        </div>
-      )
-    default:
-      return <p>디폴트</p>
-  }
-}
-const MainPage = ({ type }: MatchProps) => {
-  return <div>{getBGStyle('BEFORE')}</div>
-}
-export default MainPage
--- src/app/_components/common/NavBar.tsx
+++ src/app/_components/common/NavBar.tsx
@@ -10,41 +10,47 @@ interface TextProps {
 const getTextStyle = (type: string) => {
   switch (type) {
     case 'BEFORE':
-      return (
-        <div className="pt-11 h-[223px] rounded-[88px] translate-y-1/3 flex justify-between bg-[#E6E6E6]">
-          <div className="ml-16">
-            <Icons name={deactivation} />
-          </div>
-          <div className="bg-[#FEB3B8] rounded-full w-[72px] h-[72px] -mt-16">
-            <Icons name={chat} className="ml-6 mt-6" />
-          </div>
-          <div className="mr-16">
-            <Icons name={bio} />
-          </div>
-        </div>
-      )
+      return {
+        box: 'bg-[#E6E6E6]',
+        chat: 'bg-[#FEB3B8]',
+        chating: chat,
+      }
     case 'AFTER':
-      return (
-        <div className="pt-11 h-[223px] rounded-[88px] translate-y-1/3 flex justify-between bg-[#FFFFFF]">
-          <div className="ml-16">
-            <Icons name={deactivation} />
-          </div>
-          <div className="bg-[#FC4F59] rounded-full w-[72px] h-[72px] -mt-16">
-            <Icons name={whitechat} className="ml-6 mt-6" />
-          </div>
-          <div className="mr-16">
-            <Icons name={bio} />
-          </div>
-        </div>
-      )
+      return {
+        box: 'bg-[#FFFFFF]',
+        chat: 'bg-[#FC4F59]',
+        chating: whitechat,
+      }
     default:
-      return <p>디폴트</p>
+      return {
+        box: '',
+        chat: '',
+        chating: chat,
+      }
   }
 }
 const NavBar = ({ type }: TextProps) => {
   return (
     <div className="w-full transform fixed bottom-0 left-1/2 -translate-x-1/2 ">
-      {getTextStyle(type)}
+      <div
+        className={`${
+          getTextStyle(type).box
+        } pt-11 h-[223px] rounded-[88px] translate-y-1/3 flex justify-between`}
+      >
+        <div className="ml-16">
+          <Icons name={deactivation} />
+        </div>
+        <div
+          className={`${
+            getTextStyle(type).chat
+          } rounded-full w-[72px] h-[72px] -mt-16`}
+        >
+          <Icons name={getTextStyle(type).chating} className="ml-6 mt-6" />
+        </div>
+        <div className="mr-16">
+          <Icons name={bio} />
+        </div>
+      </div>
     </div>
   )
 }
