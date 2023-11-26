834e12f501292cb201000843643ecf1a4e4c1c16
nayoung3669 <skdud3669@gmail.com>
2023-11-26 23:20:27 +0900

    :sparkles: [feat] 날짜 바뀔 때 마다 year/month/day 생성

--- src/app/_components/chat/chatroom/ChatList.tsx
+++ src/app/_components/chat/chatroom/ChatList.tsx
@@ -1,26 +1,44 @@
+import { displayNewDate } from '@/utils/date'
 import ChatItem from './ChatItem'
+import ChatDate from './ChatDate'
 
 interface ChatListProps {
   userId: number
-  chatHistory: ChatMessageGet[]
+  chatHistory: ChatMessageFromServer[] | ChatMessageFromClient[]
 }
 
 const ChatList = ({ userId, chatHistory }: ChatListProps) => {
+  let prevDate: string | null = null
   return (
     <>
       {chatHistory.map((chat, idx) => {
-        return chat.senderId !== userId ? (
-          <ChatItem
-            key={chat.messageId}
-            type="YOU"
-            chat={chat}
-            newSender={
-              idx === 0 ||
-              (idx > 0 && chatHistory[idx - 1].senderId !== chat.senderId)
-            }
-          />
-        ) : (
-          <ChatItem key={chat.messageId} type="ME" chat={chat} />
+        const messageDate = displayNewDate(chat.createdAt)
+        let displayDate: boolean | null = null
+
+        if (messageDate !== prevDate) {
+          console.log(messageDate, '----', prevDate)
+          displayDate = true
+          prevDate = messageDate
+        }
+        return (
+          <>
+            {displayDate && <ChatDate date={messageDate} />}
+            {chat.userId !== userId ? (
+              <>
+                <ChatItem
+                  key={idx}
+                  type="YOU"
+                  chat={chat}
+                  newSender={
+                    idx === 0 ||
+                    (idx > 0 && chatHistory[idx - 1].userId !== chat.userId)
+                  }
+                />
+              </>
+            ) : (
+              <ChatItem key={idx} type="ME" chat={chat} />
+            )}
+          </>
         )
       })}
     </>
