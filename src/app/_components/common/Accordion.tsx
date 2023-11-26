f87c61e9043ac056e61587ea05cc73e9d0b69aba
One-HyeWon <5876675@naver.com>
2023-11-26 22:09:52 +0900

    :sparkles:[feat]: 아코디언 바 생성 위치 상단으로 변경

--- src/app/_components/common/Accordion.tsx
+++ src/app/_components/common/Accordion.tsx
@@ -1,45 +1,45 @@
-import { useState } from 'react'
 import { DEPARTMENT_LIST } from '@/_constants/info'
 
 interface AccordionProps {
+  selectedDepartment: string
   onDepartmentSelect: (department: string) => void
-  className?: string
-  itemClassName?: string
-  buttonClassName?: string
+  onOpen: () => void
+  isOpen: boolean
 }
 
 const Accordion = ({
+  selectedDepartment,
   onDepartmentSelect,
-  className,
-  itemClassName,
-  buttonClassName,
+  onOpen,
+  isOpen,
 }: AccordionProps) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [selectedDepartment, setSelectedDepartment] =
-    useState('학과를 선택하세요')
-
-  const handleOpen = () => {
-    setIsOpen(!isOpen)
-  }
-
   const handleSelect = (department: string) => {
-    setSelectedDepartment(department)
-    setIsOpen(false)
     onDepartmentSelect(department)
   }
 
   return (
-    <div className={className}>
-      <button type="button" className={buttonClassName} onClick={handleOpen}>
+    <div
+      className={`w-72 mx-auto mt-[419px] ${
+        isOpen ? 'rounded-lg flex flex-col-reverse' : 'rounded-lg h-12'
+      } scrollbar-hide`}
+      style={{ backgroundColor: isOpen ? '#FFF3F4' : '#FFE5E7' }}
+    >
+      <button
+        type="button"
+        className={`w-full h-12 rounded-lg ${
+          selectedDepartment === '학과를 입력하세요' ? 'bg-FFE5E7' : 'bg-FC4F59'
+        } text-FC4F59`}
+        onClick={onOpen}
+      >
         {selectedDepartment}
-        <span>{isOpen ? '▼' : '▲'}</span>
+        <span className="ml-[50px]">{isOpen ? '▼' : '▲'}</span>
       </button>
       {isOpen && (
-        <div className="max-h-20 overflow-y-scroll">
+        <div className="max-h-36 overflow-y-scroll">
           {DEPARTMENT_LIST.map((department) => (
             <button
               key={department}
-              className={itemClassName}
+              className="w-full h-12 rounded-lg bg-FFF3F4"
               onClick={() => handleSelect(department)}
               type="button"
             >
