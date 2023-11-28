4e0a0f0c5521f7c9e861a9de2f567a36f2549e2a
nayoung3669 <skdud3669@gmail.com>
2023-11-28 16:18:32 +0900

    :art: [refactor] Bio 컴포넌트 사이즈 옵션 추가

--- src/app/_components/chat/chatlist/ChatPreview.tsx
+++ src/app/_components/chat/chatlist/ChatPreview.tsx
@@ -24,7 +24,7 @@ const ChatPreview = ({
     <section className="bg-white h-[80px] w-[80%] rounded-2xl ">
       <Link href={`/chat/room/${userId}/${roomId}`}>
         <div className="flex justify-center items-center flex-row gap-4 h-full">
-          <Bio gender={gender} />
+          <Bio gender={gender} size="SMALL" />
           <div className="flex flex-col w-[180px]  bg-white">
             <p className="font-bold text-[16px] line-clamp-1 ">{nickname}</p>
             <div className="text-[14px] w-[181px] line-clamp-2 leading-[1.3]">
