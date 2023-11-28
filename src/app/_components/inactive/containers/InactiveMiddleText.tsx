98dd55a1cfb2cb9f6801c6da8154321c6deb459d
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-28 22:52:10 +0900

    [refactor]: 비활성화 페이지 컴포넌트 분리

---
+++ src/app/_components/inactive/containers/InactiveMiddleText.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import React from 'react'
+import NormalButton from '@/_components/common/NormalButton'
+import { useRouter } from 'next/navigation'
+
+const InactiveFirstText = () => {
+  const route = useRouter()
+  const rematching = () => {
+    route.push('/main')
+  }
+  const moveToMypage = () => {
+    route.push('/mypage')
+  }
+  return (
+    <div className="flex flex-col items-center">
+      <NormalButton
+        buttonText="매칭 다시 시작하기"
+        onClick={rematching}
+        buttonType="large"
+        className="bg-[#FC4F59] text-white text-[14px] w-[312px] h-[48px] rounded-[8px] mt-20"
+        isActive
+        color=""
+      />
+      <NormalButton
+        buttonText="매칭 다시 시작하기"
+        onClick={moveToMypage}
+        buttonType="large"
+        className="bg-[#D6D6D6] text-[14px] w-[312px] h-[48px] rounded-[8px] mt-3"
+        isActive
+        color=""
+      />
+    </div>
+  )
+}
+
+export default InactiveFirstText
