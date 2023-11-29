16bb4c4ed4ae628f21f71a1e67a3ec918b7423c0
One-HyeWon <5876675@naver.com>
2023-11-30 05:44:52 +0900

    :recycle:[refactore]: NormalButton Link 태그 추가

--- src/app/_components/common/NormalButton.tsx
+++ src/app/_components/common/NormalButton.tsx
@@ -1,21 +1,25 @@
+'use client'
+import Link from 'next/link'
+
 interface NormalButtonProps {
   buttonText: string
   onClick: () => void
   buttonType: 'large' | 'small'
   className: string
   isActive: boolean
+  to?: string
+  useLink?: boolean
 }
 
 const getButtonStyles = (buttonType: 'large' | 'small', className: string) => {
-  const baseStyles = `${className}`
   switch (buttonType) {
     case 'large':
       return {
-        button: `w-[312px] h-[48px] ${baseStyles}`,
+        button: `w-[312px] h-[48px] ${className}`,
       }
     case 'small':
       return {
-        button: `w-[96px] h-[36px] ${baseStyles}`,
+        button: `w-[96px] h-[36px] ${className}`,
       }
     default:
       return {
@@ -24,24 +28,34 @@ const getButtonStyles = (buttonType: 'large' | 'small', className: string) => {
   }
 }
 
+// 'use client' and imports...
+
 const NormalButton = ({
   onClick,
   buttonText,
   buttonType,
   className,
   isActive,
+  to,
+  useLink,
 }: NormalButtonProps) => {
   const buttonStyles = getButtonStyles(buttonType, className)
 
-  return (
+  const ButtonComponent = (
     <button
       type="button"
       className={`${buttonStyles.button}`}
-      onClick={onClick}
+      onClick={useLink ? undefined : onClick}
       disabled={!isActive}
     >
       {buttonText}
     </button>
+  )
+
+  return useLink && to ? (
+    <Link href={to}>{ButtonComponent}</Link>
+  ) : (
+    ButtonComponent
   )
 }
 
