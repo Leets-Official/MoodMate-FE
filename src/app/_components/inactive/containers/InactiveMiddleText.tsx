dbbc8bf789a22728c07c2bcd986fd9d5a50ec92c
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-30 23:04:14 +0900

    [refactor]: 버튼텍스트 객체만들기

--- src/app/_components/inactive/containers/InactiveMiddleText.tsx
+++ src/app/_components/inactive/containers/InactiveMiddleText.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import NormalButton from '@/_components/common/NormalButton'
 import { useRouter } from 'next/navigation'
+import { INACTIVE_BUTTON } from '@/_constants'
 
 const InactiveFirstText = () => {
   const route = useRouter()
@@ -15,14 +16,14 @@ const InactiveFirstText = () => {
   return (
     <div className="flex flex-col items-center">
       <NormalButton
-        buttonText="매칭 다시 시작하기"
+        buttonText={INACTIVE_BUTTON.REMATCH}
         onClick={rematching}
         buttonType="large"
         className="bg-[#FC4F59] text-white text-[14px] w-[312px] h-[48px] rounded-[8px] mt-20"
         isActive
       />
       <NormalButton
-        buttonText="마이페이지로 돌아가기"
+        buttonText={INACTIVE_BUTTON.GOMAIN}
         onClick={moveToMypage}
         buttonType="large"
         className="bg-[#D6D6D6] text-[14px] w-[312px] h-[48px] rounded-[8px] mt-3"
