8ccaad3b86719913a1d171c3457a81e803d84c26
은지 <81403265+eunji0714@users.noreply.github.com>
2023-11-30 11:34:14 +0900

    :lipstick: [refactor]: 한줄띄 우기

--- src/app/_constants/main.ts
+++ src/app/_constants/main.ts
@@ -6,5 +6,6 @@ export const BEFORE_TIMER_GUIDE = {
 
 export const AFTER_TIMER_GUIDE = {
   SUB_TITLE: '채팅 종료까지 남은 시간',
-  DESCRIPTION: '무디와 함께 메리크리스마스!!',
+  DESCRIPTION: '무디와 함께',
+  DESCRIPTIONTWO: '메리크리스마스!!',
 } as const
