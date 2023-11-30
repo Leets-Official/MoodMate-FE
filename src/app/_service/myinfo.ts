6aa84fdfea0f694f3603bffab47a7ee5e0b6a668
One-HyeWon <5876675@naver.com>
2023-11-30 14:08:07 +0900

    :sparkles:[feat]: myinfo 페이지 api 요청 코드 작성

--- src/app/_service/myinfo.ts
+++ src/app/_service/myinfo.ts
@@ -1,20 +1,5 @@
 import api from './axios'
 
-interface UserInfoData {
-  nickname: string
-  gender: '남' | '여'
-  age: number
-  department: string
-  keywords: string[]
-}
-
-interface PreferInfoData {
-  ageMin: number
-  ageMax: number
-  departmentPossible: boolean
-  mood: string
-}
-
 export const postUserInfo = async (userInfo: UserInfoData) => {
   try {
     const response = await api.post('/users/user-info', userInfo)
