2d1f738f089cfa22d4e38cad5f607808d2254851
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-02 14:30:40 +0900

    [refactor]: 메인페이지 data get 성공

--- src/app/_service/main.ts
+++ src/app/_service/main.ts
@@ -1,27 +1,11 @@
 import api from './axios'
 
 /** 메인페이지 내용 가져오기 */
-export const mainInfo = async (
-  userId: number,
-  userGender: string,
-  userMatchActive: boolean,
-  roomId: number,
-  roomActive: boolean,
-) => {
+export const mainInfo = async () => {
   try {
-    return await api
-      .get('/users/main', {
-        params: {
-          userId: userId.toString(),
-          userGender: userGender.toString(),
-          userMatchActive: userMatchActive.toString(),
-          roomId: roomId.toString(),
-          roomActive: roomActive.toString(),
-        },
-      })
-      .then((res) => res.data)
-  } catch (e: any) {
-    console.log('메인페이지 api 가져오기 : ', e.message)
-    throw e
+    return await api.get('/users/main').then((res) => res.data)
+  } catch (error) {
+    console.log('메인페이지 api 가져오기 : ', error)
+    throw error
   }
 }
