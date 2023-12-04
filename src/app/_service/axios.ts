527ef03ad26076c865cdb551de7b2bbb556e6b7e
nayoung3669 <skdud3669@gmail.com>
2023-12-05 00:58:22 +0900

    [refactor] : refactor for merging

--- src/app/_service/axios.ts
+++ src/app/_service/axios.ts
@@ -1,32 +1,21 @@
 import axios, { InternalAxiosRequestConfig } from 'axios'
-import { parseCookies, setCookie } from 'nookies'
-import { GOOGLE_LOGIN } from '@/_lib/google'
-
-const getAccessToken = () => {
-  const cookies = parseCookies()
-  return cookies.accessToken
-}
-
-const getRefreshToken = () => {
-  const cookies = parseCookies()
-  return cookies.refreshToken
-}
+import Cookies from 'js-cookie'
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL, // server url 변경!
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
 })
 
 export const loginApi = axios.create({
-  baseURL: GOOGLE_LOGIN,
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
 })
 
 api.interceptors.request.use(
   async (config: InternalAxiosRequestConfig) => {
-    const token = getAccessToken()
-    console.log('ddddddddd', token)
-    if (token) {
+    const accessToken = Cookies.get('accessToken')
+    console.log(accessToken)
+    if (accessToken) {
       // eslint-disable-next-line no-param-reassign
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${accessToken}`
     }
     return config
   },
@@ -35,33 +24,33 @@ api.interceptors.request.use(
   },
 )
 
-api.interceptors.response.use(
-  (response) => {
-    return response
-  },
-  async (error) => {
-    const originalRequest = error.config
-    const refreshToken = getRefreshToken()
-    if (
-      error.response.status === 401 &&
-      originalRequest &&
-      !error.config.__isRetryRequest &&
-      refreshToken
-    ) {
-      try {
-        const newAccessToken = await axios.post(
-          `${process.env.NEXT_PUBLIC_SERVER_URL}/users/refresh`,
-          {
-            refreshToken,
-          },
-        )
-        setCookie(null, 'accessToken', newAccessToken.data.accessToken, {
-          maxAge: 3 * 60 * 60,
-          path: '/',
-        })
-      } catch (e) {}
-    }
-  },
-)
+// api.interceptors.response.use(
+//   (response) => {
+//     return response
+//   },
+//   async (error) => {
+//     const originalRequest = error.config
+//     const refreshToken = Cookies.get('refreshToken')
+//     if (
+//       error.response.status === 401 &&
+//       originalRequest &&
+//       !error.config.__isRetryRequest &&
+//       refreshToken
+//     ) {
+//       try {
+//         // const newAccessToken = await axios.post(
+//         //   `${process.env.NEXT_PUBLIC_SERVER_URL}/users/refresh`,
+//         //   {
+//         //     refreshToken,
+//         //   },
+//         // )
+//         // setCookie(null, 'accessToken', newAccessToken.data.accessToken, {
+//         //   maxAge: 3 * 60 * 60,
+//         //   path: '/',
+//         // })
+//       } catch (e) {}
+//     }
+//   },
+// )
 
 export default api
