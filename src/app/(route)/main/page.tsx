9e1c7636aa73fdea00b6bb46fa72dfac7e320e5f
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-05 14:31:36 +0900

    [refactor]: loading page

--- src/app/(route)/main/page.tsx
+++ src/app/(route)/main/page.tsx
@@ -4,11 +4,12 @@ import MainPage from '@/_components/main/MainPage'
 import { useMainQuery } from '@/_hooks/useMainQuery'
 import React, { useEffect } from 'react'
 import InactivePage from '@/_components/inactive/InActivePage'
+import Loading from '@/_components/common/Loading'
 
 export default function MainpagePage() {
   const { isLoading, isError, data } = useMainQuery()
   if (isLoading) {
-    return <div>Loading...</div>
+    return <Loading />
   }
   if (isError || !data) {
     return <div>Error occurred while fetching data</div>
