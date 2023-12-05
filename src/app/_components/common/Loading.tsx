e892afbd1df33ec8888855cf79d10dd435ccbbd2
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-05 14:40:39 +0900

    [refacotr]: laoading page 최종

--- src/app/_components/common/Loading.tsx
+++ src/app/_components/common/Loading.tsx
@@ -3,8 +3,12 @@ import Image from 'next/image'
 
 const Loading = () => {
   return (
-    <div className="bg-primary w-full h-full">
-      <Image src={loading} alt="로딩아이콘" />
+    <div className="bg-primary w-full h-screen flex">
+      <Image
+        src={loading}
+        alt="로딩아이콘"
+        className="w-[142px] h-[108px] my-auto mx-auto"
+      />
     </div>
   )
 }
