3c896c313d9321ae231615035dbbe7df96d04adb
One-HyeWon <5876675@naver.com>
2023-12-07 01:29:43 +0900

    :lipstick:[refactor]: GENDER 타입 버튼 크기 변경

--- src/app/_constants/style.ts
+++ src/app/_constants/style.ts
@@ -1,5 +1,5 @@
 export const BUTTON_STYLE = {
-  GENDER: (className: string) => `w-[148px] h-[166px] ${className}`,
+  GENDER: (className: string) => `w-[147px] h-[164px] ${className}`,
   KEYWORD: (className: string) => `${className}`,
   MOOD: (className: string) => `w-[148px] h-[166px] ${className}`,
   MAJOR: (className: string) => `w-[148px] h-[166px] ${className}`,
