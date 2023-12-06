2d1ed8271a4a4e5bf3cd97d2a15732e0c4c95780
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-06 16:50:35 +0900

    login test

--- src/app/_service/axios.ts
+++ src/app/_service/axios.ts
@@ -12,16 +12,16 @@ const api = axios.create({
 api.interceptors.request.use(
   async (config: InternalAxiosRequestConfig) => {
     // const token = getAccessToken()
-    const accessToken = Cookies.get('accessToken')
-    const refreshToken = Cookies.get('refreshToken')
-
-    console.log('access', accessToken)
-    console.log('refresh', refreshToken)
-    // console.log('docum', document.cookie)
-
-    // if (token) {
-    // eslint-disable-next-line no-param-reassign
-    config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiUk9MRV9VU0VSIiwiaWQiOjQsImVtYWlsIjoiZXVuenp6enp6MUBuYXZlci5jb20iLCJzdWIiOiI0IiwiZXhwIjoxNzAxODUyOTY1fQ.rbF2BOPnUgv7rlgL6W8aozFuPQKRrkdJc7-rerQhjBQ`
+    // const accessToken = Cookies.get('accessToken')
+    // const refreshToken = Cookies.get('refreshToken')
+    //
+    // console.log('access', accessToken)
+    // console.log('refresh', refreshToken)
+    // // console.log('docum', document.cookie)
+    //
+    // // if (token) {
+    // // eslint-disable-next-line no-param-reassign
+    // config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiUk9MRV9VU0VSIiwiaWQiOjQsImVtYWlsIjoiZXVuenp6enp6MUBuYXZlci5jb20iLCJzdWIiOiI0IiwiZXhwIjoxNzAxODUyOTY1fQ.rbF2BOPnUgv7rlgL6W8aozFuPQKRrkdJc7-rerQhjBQ`
     // }
     return config
   },
