6b17e0ffb02b6b70c522be347d1110d5b0c8888b
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-06 23:06:41 +0900

    login test

--- src/app/_service/main.ts
+++ src/app/_service/main.ts
@@ -3,7 +3,7 @@ import api from './axios'
 /** 메인페이지 내용 가져오기 */
 export const mainInfo = async () => {
   try {
-    return await api.get('/users/main').then((res) => res.data)
+    return await api.post('/users/main').then((res) => res.data)
   } catch (error) {
     console.log('메인페이지 api 가져오기 : ', error)
     throw error
