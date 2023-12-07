8b9818ba355181ff4f8807c4d8f096d35e7a405c
One-HyeWon <5876675@naver.com>
2023-12-07 11:16:56 +0900

    :sparkles:[feat]: 학과 선택 페이지

--- src/app/_components/common/userinfo/UserDepartment.tsx
+++ src/app/_components/common/userinfo/UserDepartment.tsx
@@ -1,3 +1,67 @@
-export default function UserDepartment() {
-  return <div>Department page</div>
+import { MY_DEPARTMENT_PAGE } from '@/_constants/info'
+import Accordion from '../Accordion'
+import NormalButton from '../NormalButton'
+import { userInfoState } from '@/_atom/userinfo'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useRecoilState } from 'recoil'
+
+interface UserDepartmentProps {
+  pageNum: string
+}
+
+export default function UserDepartment({ pageNum }: UserDepartmentProps) {
+  const route = useRouter()
+
+  const [userInfo, setUserInfoState] = useRecoilState(userInfoState)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const handleDepartmentSelect = (department: string) => {
+    setIsOpen(false)
+
+    setUserInfoState((prev) => ({ ...prev, department: department }))
+  }
+
+  const nextRoute = () => {
+    route.push(`/userinfo/${parseInt(pageNum, 10) + 1}`)
+    console.log(userInfo)
+  }
+
+  const handleAccordionOpen = () => {
+    setIsOpen((prev) => !prev)
+  }
+
+  const buttonStyles = {
+    defaultStyles: 'bg-secondary',
+    activeStyles: 'text-white bg-[#FC4F59]',
+  }
+
+  return (
+    <div className="relative h-[560px] w-[312px]">
+      <div className="mt-[35px] mb-[168px]">
+        <div className="leading-normal text-darkgray font-bold text-2xl font-sans">
+          {MY_DEPARTMENT_PAGE.GREETINGS}
+        </div>
+      </div>
+      <div>
+        <Accordion
+          selectedDepartment={userInfo.department}
+          onDepartmentSelect={handleDepartmentSelect}
+          onOpen={handleAccordionOpen}
+          isOpen={isOpen}
+        />
+      </div>
+      <NormalButton
+        buttonText="다음"
+        onClick={nextRoute}
+        buttonType="large"
+        className={`absolute bottom-0 text-darkgray rounded-md ${
+          userInfo.department
+            ? buttonStyles.activeStyles
+            : buttonStyles.defaultStyles
+        }`}
+        isActive={!!userInfo.department}
+      />
+    </div>
+  )
 }
