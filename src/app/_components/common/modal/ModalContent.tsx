d167aa6a6f29688974585ccccdf3e6c6c7d11b11
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-07 22:44:17 +0900

    modal test

--- src/app/_components/common/modal/ModalContent.tsx
+++ src/app/_components/common/modal/ModalContent.tsx
@@ -25,14 +25,12 @@ const ModalContent = ({
           {SUB_TITLE || ''}
         </h5>
       </div>
-      <div className="w-[169px] h-[193px] bg-neutral-300">
-        {/* 나중에 dynamic으로 가져오기?, 크기 조정 */}
-        {onMyPage ? (
-          <p>씩씩한 무디 이미지</p>
-        ) : (
-          <Image src={inactive} alt="inactive" />
-        )}
-      </div>
+      {/* 나중에 dynamic으로 가져오기?, 크기 조정 */}
+      {onMyPage ? (
+        <p>씩씩한 무디 이미지</p>
+      ) : (
+        <Image src={inactive} alt="inactive" className="w-[142px] h-[197px]" />
+      )}
       <ModalButtons
         onConfirm={onConfirm}
         onCancel={onCancel}
