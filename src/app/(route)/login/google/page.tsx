dd2c42834670b214e85e1878d60c21eb418a3f3d
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-07 11:36:59 +0900

    login test

--- src/app/(route)/login/google/page.tsx
+++ src/app/(route)/login/google/page.tsx
@@ -2,33 +2,31 @@
 
 import React, { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
-import { router } from 'next/client'
+import { useRouter } from 'next/navigation'
 
 const OauthPage = () => {
   const [accessToken, setAccessToken] = useState<string>('')
   const [refreshToken, setRefreshToken] = useState<string>('')
+  const router = useRouter()
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const refreshTokenURL = new URL(window.location.href).searchParams.get(
-        'refreshToken',
-      )
-      const accessTokenURL = new URL(window.location.href).searchParams.get(
-        'accessToken',
-      )
+      const refreshTokenURL = new URL(window.location.href).searchParams.get('refreshToken')
+      const accessTokenURL = new URL(window.location.href).searchParams.get('accessToken')
       const splitToken = accessTokenURL?.split('?')
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      setAccessToken(splitToken[0])
+
+      if (splitToken && splitToken.length > 0) {
+        setAccessToken(splitToken[0])
+      }
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       setRefreshToken(refreshTokenURL)
     }
-  }, [accessToken, refreshToken])
+  }, [])
   useEffect(() => {
     if (accessToken) {
       router.push('/main')
     }
-  }, [accessToken])
+  }, [accessToken, router])
   Cookies.set('realAccessToken', accessToken)
   Cookies.set('realRefreshToken', refreshToken)
   return (
