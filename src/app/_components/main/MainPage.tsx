c531bd96abd5eecda758366497caa123504386e2
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-07 18:42:10 +0900

    [refactor] navbar 추가

--- src/app/_components/main/MainPage.tsx
+++ src/app/_components/main/MainPage.tsx
@@ -67,6 +67,12 @@ const MainPage = ({ type }: MatchProps) => {
         className="w-[156px] mx-auto mt-24 desktop:mt-4"
       />
       <Image src={beforeMatch} alt="beforeMatch" className="-mt-16" />
+      <NavBar
+        type={updatedType}
+        roomId={roomId}
+        userId={userId}
+        roomActive={roomActive}
+      />
     </div>
   )
 }
