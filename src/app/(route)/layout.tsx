ddc53317a6cf744027e1b87216392ee86895d7a7
nayoung3669 <skdud3669@gmail.com>
2023-12-10 23:38:55 +0900

    :recycle: [refactor] : layout 위치 변경, favicon

--- src/app/(route)/layout.tsx
+++ src/app/(route)/layout.tsx
@@ -5,6 +5,9 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: '무드메이트',
   description: '하루 한번, 나와 비슷한 무드의 상대방을 매칭해보세요!',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
