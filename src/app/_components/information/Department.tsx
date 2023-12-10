21c27ce1e75bfaea98626f2162ad71900c3242a5
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-10 21:53:23 +0900

    [refactor] 선호 학과 컴포넌트 수정

--- src/app/_components/information/Department.tsx
+++ src/app/_components/information/Department.tsx
@@ -2,17 +2,12 @@ interface DepartmentProps {
   department: boolean
 }
 const Department = ({ department }: DepartmentProps) => {
+  const text = department ? '같은학과가 좋아요' : '다른학과가 좋아요'
   return (
     <div className="flex text-[14px] ml-3 my-2">
-      ({department} ?
       <p className="px-5 py-1.5 rounded-[18px] mr-2 bg-[#FFE5E7] text-[#FC4F59] border border-[#FC4F59]">
-        같은학과가 좋아요
+        {text}
       </p>
-      :
-      <p className="px-5 py-1.5 rounded-[18px] mr-2 bg-[#FFE5E7] text-[#FC4F59] border border-[#FC4F59]">
-        다른학과가 좋아요
-      </p>
-      )
     </div>
   )
 }
