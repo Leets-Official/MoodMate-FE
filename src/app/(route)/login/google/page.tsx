cdf098347064510412df48441b40194658719fb5
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-10 19:18:22 +0900

    [refactor] router test

--- src/app/(route)/login/google/page.tsx
+++ src/app/(route)/login/google/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/navigation'
 import Loading from '@/_components/common/Loading'
+import { useMainQuery } from '@/_hooks/useMainQuery'
 
 const OauthPage = () => {
   const [accessToken, setAccessToken] = useState<string>('')
@@ -26,13 +27,29 @@ const OauthPage = () => {
       setRefreshToken(refreshTokenURL)
     }
   }, [])
-  useEffect(() => {
-    if (accessToken) {
-      router.push('/main')
-    }
-  }, [accessToken, router])
+  // useEffect(() => {
+  //   if (accessToken) {
+  //     router.push('/main')
+  //   }
+  // }, [accessToken, router])
   Cookies.set('realAccessToken', accessToken)
   Cookies.set('realRefreshToken', refreshToken)
+  const { isLoading, isError, data } = useMainQuery()
+  if (isLoading) {
+    return <Loading />
+  }
+  if (isError || !data) {
+    return <div>Error...</div>
+  }
+  const { userGender } = data.mainPageResponse
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    if (userGender === 'MALE' || userGender === 'FEMALE') {
+      router.push('/main')
+    } else {
+      router.push('/userinfo/1')
+    }
+  }, [router, userGender])
   return (
     <div>
       <Loading />
