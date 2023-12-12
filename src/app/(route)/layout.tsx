292a4a244a003990dc2d4361b407bd16ce672ce3
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-12 19:14:32 +0900

    main page ui test

--- src/app/(route)/layout.tsx
+++ src/app/(route)/layout.tsx
@@ -19,11 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="h-screen desktop:w-[360px] desktop:mx-auto">
         <RecoilProvider>
-          <div className="h-[5%]" />
-          <div className="h-[90%]">
             <QueryProvider>{children}</QueryProvider>
-          </div>
-          <div className="h-[5%]" />
         </RecoilProvider>
       </body>
     </html>
