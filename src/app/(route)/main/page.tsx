959837fdd78ff9af0fdc2333345d3ed9af1e3dce
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-12 17:05:07 +0900

    [refactor] mainpage ui

--- src/app/(route)/main/page.tsx
+++ src/app/(route)/main/page.tsx
@@ -2,10 +2,9 @@
 
 import MainPage from '@/_components/main/MainPage'
 import { useMainQuery } from '@/_hooks/useMainQuery'
-import React, { useEffect } from 'react'
+import React from 'react'
 import InactivePage from '@/_components/inactive/InActivePage'
 import Loading from '@/_components/common/Loading'
-import Cookies from 'js-cookie'
 
 export default function MainpagePage() {
   const { isLoading, isError, data } = useMainQuery()
