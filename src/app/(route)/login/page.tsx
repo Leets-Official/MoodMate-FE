da08e8c279893ed54e0f78568f0218a51c633622
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-12 18:28:56 +0900

    [refactor] login page ui

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -11,26 +11,30 @@ export default function Login() {
     window.location.href = `${process.env.GOOGLE_LOGIN}oauth/login/google`
   }
   return (
-    <div className="flex flex-col">
-      <div className="flex font-bold text-[20px] mt-36 ml-12 desktop:mt-24">
-        {LOGIN_PAGE.FIRST}
-        <div className="ml-1.5 text-primary">{LOGIN_PAGE.SECOND}</div>
-        {LOGIN_PAGE.THIRD}
+    <div className="flex flex-col h-screen mx-5">
+      <div className="h-[20%]">
+        <div className="flex font-bold text-[20px] ml-5 mt-[20%] desktop:mt-[20%]">
+          {LOGIN_PAGE.FIRST}
+          <div className="ml-1.5 text-primary">{LOGIN_PAGE.SECOND}</div>
+          {LOGIN_PAGE.THIRD}
+        </div>
+        <div className="flex font-bold text-[20px] ml-5">
+          <div className="text-primary">{LOGIN_PAGE.FOUR}</div>
+          {LOGIN_PAGE.FIVE}
+        </div>
       </div>
-      <div className="flex font-bold text-[20px] ml-12">
-        <div className="text-primary">{LOGIN_PAGE.FOUR}</div>
-        {LOGIN_PAGE.FIVE}
+      <div className="h-[50%]">
+        <Image
+          src={loginImage}
+          alt="loginImage"
+          className="w-full mt-10 mb-24 desktop:mt-10 desktop:mb-14"
+        />
       </div>
-      <Image
-        src={loginImage}
-        alt="loginImage"
-        className="w-[297px] mt-24 mb-24 mx-auto desktop:mt-10 desktop:mb-14"
-      />
       <Image
         src={google}
         alt="구글로그인버튼"
         onClick={handleLogin}
-        className="hover:cursor-pointer w-[312px] mx-auto"
+        className="hover:cursor-pointer mt-8 w-full mx-auto"
       />
     </div>
   )
