e332ea409221bdf936d695d13d80cd6eca22da1c
nayoung3669 <skdud3669@gmail.com>
2023-12-14 15:36:08 +0900

    :recycle: [refactor] : 스크롤

--- src/app/_components/chat/containers/ChatRoomContainer.tsx
+++ src/app/_components/chat/containers/ChatRoomContainer.tsx
@@ -14,9 +14,9 @@ interface ChatRoomContainerProps {
 }
 const ChatRoomContainer = ({ userId, roomId }: ChatRoomContainerProps) => {
   const scrollRef = useRef<HTMLDivElement>(null)
-  const [scrollHeight, setScrollHeight] = useState(0)
   const topDivRef = useRef<HTMLDivElement>(null)
   const containerRef = useRef<HTMLDivElement>(null)
+  const [scrollHeight, setScrollHeight] = useState(0)
 
   const [realTimeMessages, setRealTimeMessages] = useRecoilState(
     realTimeMessagesState,
@@ -25,56 +25,57 @@ const ChatRoomContainer = ({ userId, roomId }: ChatRoomContainerProps) => {
     useInfiniteChatQuery(userId, roomId, CHAT_SIZE.ROOM)
 
   useEffect(() => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight
     }
   }, [realTimeMessages])
 
   useEffect(() => {
-    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting && hasNextPage && !isFetchingNextPage) {
-          fetchNextPage()
-        }
-      })
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight
+    }
+  }, [])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const container = containerRef.current
+      if (
+        container &&
+        container.scrollTop === 0 &&
+        hasNextPage &&
+        !isFetchingNextPage
+      ) {
+        fetchNextPage()
+      }
     }
 
-    const observer = new IntersectionObserver(handleIntersection, {
-      root: null,
-      rootMargin: '0px',
-      threshold: 1.0,
-    })
-
-    if (
-      scrollRef.current &&
-      scrollRef.current.scrollHeight - scrollRef.current.scrollTop ===
-        scrollRef.current.clientHeight &&
-      hasNextPage
-    ) {
-      observer.observe(topDivRef.current!)
+    if (containerRef.current) {
+      containerRef.current.addEventListener('scroll', handleScroll)
     }
 
     return () => {
-      observer.disconnect()
+      if (containerRef.current) {
+        containerRef.current.removeEventListener('scroll', handleScroll)
+      }
     }
   }, [userId, hasNextPage, isFetchingNextPage, fetchNextPage])
 
   useEffect(() => {
-    if (!containerRef) return
-
     if (containerRef.current) {
-      const scrollTop = containerRef.current.scrollHeight - scrollHeight
-      containerRef.current.scrollTop = scrollTop
-      setScrollHeight(containerRef.current.scrollHeight)
+      containerRef.current.scrollTop = containerRef.current.scrollHeight
     }
-    console.log(data?.pages)
-  }, [data?.pages, scrollHeight])
+  }, [realTimeMessages])
 
-  useEffect(() => {
-    if (topDivRef.current) {
-      topDivRef.current.style.display = 'block'
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (!containerRef) return
+
+  //   if (containerRef.current) {
+  //     const scrollTop = containerRef.current.scrollHeight - scrollHeight
+  //     containerRef.current.scrollTop = scrollTop
+  //     setScrollHeight(containerRef.current.scrollHeight)
+  //   }
+  //   console.log(data?.pages)
+  // }, [data?.pages, scrollHeight])
 
   return (
     <section
@@ -101,3 +102,38 @@ const ChatRoomContainer = ({ userId, roomId }: ChatRoomContainerProps) => {
 }
 
 export default ChatRoomContainer
+
+// useEffect(() => {
+//   const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+//     entries.forEach((entry) => {
+//       if (
+//         entry.isIntersecting &&
+//         containerRef.current &&
+//         containerRef.current.scrollTop === 0 &&
+//         hasNextPage &&
+//         !isFetchingNextPage
+//       ) {
+//         fetchNextPage()
+//       }
+//     })
+//   }
+
+//   const observer = new IntersectionObserver(handleIntersection, {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 1.0,
+//   })
+
+//   if (
+//     scrollRef.current &&
+//     scrollRef.current.scrollHeight - scrollRef.current.scrollTop ===
+//       scrollRef.current.clientHeight &&
+//     hasNextPage
+//   ) {
+//     observer.observe(containerRef.current!)
+//   }
+
+//   return () => {
+//     observer.disconnect()
+//   }
+// }, [userId, hasNextPage, isFetchingNextPage, fetchNextPage])
