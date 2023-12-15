4dafa4fe636bff6c10834aa757e102e5b418b0eb
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-15 21:33:31 +0900

    test

--- src/app/(route)/not-found.tsx
+++ src/app/(route)/not-found.tsx
@@ -3,7 +3,7 @@ import NotfoundpageSecondContainer from '@/_components/notfoundpage/containers/N
 import Image from 'next/image'
 import errorImage from 'public/illustration/common/error/notfound.png'
 
-export default function NotFound() {
+export default function NotFoundPage() {
   return (
     <section className="w-full h-screen flex items-center justify-center">
       <Image src={errorImage} alt="image" />
