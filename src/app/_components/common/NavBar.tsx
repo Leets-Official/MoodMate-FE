06111630a6efd9e539f08bdbf20fa63005839cc7
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-15 15:47:00 +0900

    resolved

--- src/app/_components/common/NavBar.tsx
+++ src/app/_components/common/NavBar.tsx
@@ -100,7 +100,7 @@ const NavBar = ({ type, userId, roomId, roomActive, gender }: TextProps) => {
         <ModalPortal nodeName="mainPortal">
           <ModalOutside
             onClose={() => setOpenModal(false)}
-            className="max-w-md scroll overflow-hidden bg-white w-[260px] h-[467px] px-10 rounded-[25px] shadow-sm py-10 z-20 pointer-events-auto"
+            className="max-w-md scroll overflow-hidden bg-white w-[260px] h-[467px] px-10 rounded-[25px] shadow-sm py-10 z-50 pointer-events-auto"
           >
             <ModalContent
               subject={INACTIVE_MODAL}
@@ -115,7 +115,7 @@ const NavBar = ({ type, userId, roomId, roomActive, gender }: TextProps) => {
         <ModalPortal nodeName="mainPortal">
           <ModalOutside
             onClose={() => setChatModal(false)}
-            className="max-w-md scroll overflow-hidden bg-white w-[260px] h-[467px] px-10 rounded-[25px] shadow-sm z-20 pointer-events-auto"
+            className="max-w-md scroll overflow-hidden bg-white w-[260px] h-[467px] px-10 rounded-[25px] shadow-sm z-50 pointer-events-auto"
           >
             <ModalContentOne
               onClose={onChatCloseModal}
