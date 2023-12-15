3508c5438e3caa17bec98c5cca1d29710bfde5c7
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-15 14:42:36 +0900

    [refactor] z-index 수정

--- src/app/_components/common/modal/ModalOutside.tsx
+++ src/app/_components/common/modal/ModalOutside.tsx
@@ -8,7 +8,7 @@ const ModalOutside = ({ children, onClose, className }: ModalOutsideProps) => {
   return (
     <div className="fixed top-0 left-0 flex justify-center items-center w-full h-full">
       <div
-        className="absolute top-0 left-0 w-full h-full bg-[#d9d9d980] backdrop-blur-[1px] z-50"
+        className="absolute top-0 left-0 w-full h-full bg-[#d9d9d980] backdrop-blur-[1px]"
         onClick={(e) => e.target === e.currentTarget && onClose()}
         role="presentation"
       />
