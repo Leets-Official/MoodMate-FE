501c24e1b4333b395912ef889e62bc643af3497e
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-16 02:31:40 +0900

    error page

--- src/app/_components/errorpage/containers/ErrorpageSecondContainer.tsx
+++ src/app/_components/errorpage/containers/ErrorpageSecondContainer.tsx
@@ -5,7 +5,7 @@ const ErrorpageSecondContainer = () => {
     <div className="flex text-center">
       <Link
         href="/main"
-        className="mt-24 mx-auto pt-1.5 bg-[#FFE5E7] w-[146px] h-[36px] rounded-[8px] text-[#666666]"
+        className="mt-12 mx-auto pt-1.5 bg-[#FFE5E7] w-[146px] h-[36px] rounded-[8px] text-[#666666]"
       >
         새로고침
       </Link>
