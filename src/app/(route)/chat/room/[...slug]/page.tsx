b809cc4ec5f66807fd74954388f2e476773a83f6
nayoung3669 <skdud3669@gmail.com>
2023-12-15 19:14:51 +0900

    :recycle: [refactor] : chat height

--- src/app/(route)/chat/room/[...slug]/page.tsx
+++ src/app/(route)/chat/room/[...slug]/page.tsx
@@ -1,6 +1,8 @@
+'use client'
 import ChatInputContainer from '@/_components/chat/containers/ChatInputContainer'
 import ChatRoomContainer from '@/_components/chat/containers/ChatRoomContainer'
 import ChatHeader from '@/_components/layout/ChatHeader'
+import { useEffect, useRef } from 'react'
 
 interface ChatRoomPageProps {
   params: {
@@ -10,10 +12,17 @@ interface ChatRoomPageProps {
 
 export default function ChatRoomPage({ params }: ChatRoomPageProps) {
   const [userId, roomId] = params.slug
+  const pageRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (pageRef.current) {
+      pageRef.current.scrollTop = pageRef.current.scrollHeight
+    }
+  }, [])
 
   return (
-    <section className="scrollbar-hide ">
-      <div className="relative flex flex-col justify-center items-center w-full h-screen">
+    <section className="scrollbar-hide h-screen" ref={pageRef}>
+      <div className="relative flex flex-col justify-center items-center h-full w-full ">
         <ChatHeader userId={Number(userId)} />
         <ChatRoomContainer userId={Number(userId)} roomId={Number(roomId)} />
         <ChatInputContainer userId={Number(userId)} roomId={Number(roomId)} />
