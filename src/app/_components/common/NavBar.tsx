05b92836af20c29121b957412840c138889027fa
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-15 17:24:11 +0900

    test

--- src/app/_components/common/NavBar.tsx
+++ src/app/_components/common/NavBar.tsx
@@ -67,7 +67,7 @@ const NavBar = ({ type, userId, roomId, roomActive, gender }: TextProps) => {
   }
   console.log('roomActive', roomActive)
   return (
-    <div className="fixed translate-y-[43px] bottom-0 left-1/2 -translate-x-1/2 desktop:w-[378px] w-full">
+    <div className="fixed translate-y-[43px] bottom-0 left-1/2 -translate-x-1/2 desktop:w-[378px] w-full z-[10]">
       <div
         className={`${
           getTextStyle(type).box
