29e9e41a099d7078a14079070b4ede4c05aac111
nayoung3669 <skdud3669@gmail.com>
2023-12-15 18:49:59 +0900

    :recycle: [refactor] : chat recoil 수정

--- src/app/(route)/chat/room/[...slug]/page.tsx
+++ src/app/(route)/chat/room/[...slug]/page.tsx
@@ -12,10 +12,12 @@ export default function ChatRoomPage({ params }: ChatRoomPageProps) {
   const [userId, roomId] = params.slug
 
   return (
-    <section className="relative flex flex-col justify-center items-center w-full overflow-hidden h-screen">
-      <ChatHeader userId={Number(userId)} />
-      <ChatRoomContainer userId={Number(userId)} roomId={Number(roomId)} />
-      <ChatInputContainer userId={Number(userId)} roomId={Number(roomId)} />
+    <section className="scrollbar-hide ">
+      <div className="relative flex flex-col justify-center items-center w-full h-screen">
+        <ChatHeader userId={Number(userId)} />
+        <ChatRoomContainer userId={Number(userId)} roomId={Number(roomId)} />
+        <ChatInputContainer userId={Number(userId)} roomId={Number(roomId)} />
+      </div>
     </section>
   )
 }
