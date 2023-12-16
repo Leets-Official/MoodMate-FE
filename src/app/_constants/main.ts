9060c6192f13853875e4d91d45fac1782e927d16
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-16 13:46:21 +0900

    [refactor] modal 문구 변경

--- src/app/_constants/main.ts
+++ src/app/_constants/main.ts
@@ -30,10 +30,8 @@ export const CHAT_MODAL = {
 } as const
 
 export const INACTIVE_CHAT_MODAL = {
-  TITLE: '재매칭을 원하시면',
-  TITLE2: '매칭 다시 시작하기를 눌러주세요',
-  SUB_TITLE: '20시 이전에 활성화를 해야',
-  SUB_TITLE2: '당일 매칭이 이루어져요.',
+  TITLE: '재매칭을 원하시면 매칭',
+  TITLE2: '다시 시작하기를 눌러주세요',
   CLOSE: '닫기',
 } as const
 
