34bae37136cd76e59d2788f9ab0fb698dea9370c
One-HyeWon <5876675@naver.com>
2023-12-18 00:21:01 +0900

    :recycle:[refactor]: progress bar 애니메이션 초안

--- src/app/_components/common/ProgressBar.tsx
+++ src/app/_components/common/ProgressBar.tsx
@@ -1,4 +1,5 @@
 import { PROGRESS_BAR } from '@/_constants/info'
+import { useEffect, useState } from 'react'
 
 interface ProgressBarProps {
   current: keyof typeof PROGRESS_BAR
@@ -6,15 +7,30 @@ interface ProgressBarProps {
 
 const ProgressBar = ({ current }: ProgressBarProps) => {
   const total = PROGRESS_BAR.MAX
-  const percentage = (parseInt(current.toString(), 10) / total) * 100
+  const [percentage, setPercentage] = useState(
+    (parseInt(current.toString(), 10) / total) * 100,
+  )
   const currentText = PROGRESS_BAR[current]
+  const [initialRender, setInitialRender] = useState(true)
+
+  useEffect(() => {
+    setInitialRender(false)
+    setPercentage((parseInt(current.toString(), 10) / total) * 100)
+  }, [current])
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="w-[312px] bg-lightgray h-[10px] mb-[9px] rounded-full">
+      <div className="w-[312px] bg-lightgray h-[10px] mb-[9px] rounded-full relative overflow-hidden">
         <div
-          className="h-full bg-primary rounded-full"
-          style={{ width: `${percentage}%` }}
+          className={`h-full bg-primary rounded-full absolute left-0 top-0 ${
+            initialRender ? 'transition-none' : ''
+          }`}
+          style={{
+            width: `${percentage}%`,
+            transitionProperty: 'width',
+            transitionDuration: '500ms',
+            transitionTimingFunction: 'ease-in-out',
+          }}
         />
       </div>
       <div className="w-[312px] text-secondary text-[10px] flex items-center">
