0ac0dacf98fd5530cf14c4cc07e6cc83de84bd9c
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-18 01:03:25 +0900

    build error

--- src/app/_components/errorpage/containers/ErrorpageSecondContainer.tsx
+++ src/app/_components/errorpage/containers/ErrorpageSecondContainer.tsx
@@ -8,6 +8,7 @@ const ErrorpageSecondContainer = () => {
   }
   return (
     <div className="flex text-center">
+      {/* eslint-disable-next-line react/button-has-type */}
       <button
         onClick={refreshPage}
         className="mt-12 mx-auto pt-1.5 bg-[#FFE5E7] w-[146px] h-[36px] rounded-[8px] text-[#666666]"
