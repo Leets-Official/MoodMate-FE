8424fd7c82560966f78187571392dfff6fcd5c6b
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-18 01:51:11 +0900

    [refactor] route router 수정

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,20 +1,14 @@
 'use client'
 
 import Loading from '@/_components/common/Loading'
-import Cookies from 'js-cookie'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function Root() {
   const route = useRouter()
-  const accessToken = Cookies.get('accessToken')
   useEffect(() => {
-    if (accessToken) {
-      route.push('/main')
-    } else {
-      route.push('/login')
-    }
-  }, [accessToken, route])
+    route.push('/login')
+  }, [route])
   return (
     <main>
       <Loading />
