df356b50c9d67fb936868e3a088a31d483430cbc
nayoung3669 <skdud3669@gmail.com>
2023-12-17 23:52:54 +0900

    :recycle: [refactor] : refresh page

--- src/app/_components/errorpage/containers/ErrorpageSecondContainer.tsx
+++ src/app/_components/errorpage/containers/ErrorpageSecondContainer.tsx
@@ -1,14 +1,19 @@
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const ErrorpageSecondContainer = () => {
+  const router = useRouter()
+
+  const refreshPage = () => {
+    router.reload()
+  }
   return (
     <div className="flex text-center">
-      <Link
-        href="/main"
+      <button
+        onClick={refreshPage}
         className="mt-12 mx-auto pt-1.5 bg-[#FFE5E7] w-[146px] h-[36px] rounded-[8px] text-[#666666]"
       >
         새로고침
-      </Link>
+      </button>
     </div>
   )
 }
