241392aea2cc88f1633b37e2674a969a865aa7d1
은지 <81403265+eunji0714@users.noreply.github.com>
2023-12-18 01:07:16 +0900

    [feat] route page redirect

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,8 +1,19 @@
+import Loading from '@/_components/common/Loading'
+import Cookies from 'js-cookie'
+import { useRouter } from 'next/navigation'
+
 export default function Root() {
+  const route = useRouter()
+  const accessToken = Cookies.get('accessToken')
+  if (accessToken) {
+    route.push('/main')
+  } else {
+    route.push('/login')
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <p>rootpage</p>
+        <Loading />
       </div>
     </main>
   )
