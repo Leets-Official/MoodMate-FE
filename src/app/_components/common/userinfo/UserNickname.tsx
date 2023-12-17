da527c209bb075080c9f84e86979313946cdc226
One-HyeWon <5876675@naver.com>
2023-12-17 22:09:37 +0900

    :recycle:[refactor]: 닉네임 입력 조건 추가

--- src/app/_components/common/userinfo/UserNickname.tsx
+++ src/app/_components/common/userinfo/UserNickname.tsx
@@ -32,8 +32,11 @@ const UserNickname = ({ pageNum }: UserNicknameProps) => {
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value.slice(0, INPUT_NICKNAME.MAX)
-    setInputValue(newValue)
-    setinputCount(`${newValue.length}/5`)
+    const koeranOnly = /^[ㄱ-ㅎㅏ-ㅣ가-힣]*$/g
+    if (koeranOnly.test(newValue)) {
+      setInputValue(newValue)
+      setinputCount(`${newValue.length}/${INPUT_NICKNAME.MAX}`)
+    }
   }
 
   const nextRoute = () => {
@@ -73,7 +76,7 @@ const UserNickname = ({ pageNum }: UserNicknameProps) => {
 
   const inputStyles = {
     defaultStyles: 'bg-lightgray',
-    activeStyles: 'bg-primar',
+    activeStyles: 'bg-primary',
   }
 
   const buttonStyles = {
@@ -98,7 +101,7 @@ const UserNickname = ({ pageNum }: UserNicknameProps) => {
           textValue={inputValue}
           placeholder={NICK_NAME_PAGE.INPUTBOX}
           onChange={handleInputChange}
-          className="w-[240px] placeholder:text-secondary placeholder:text-base placeholder:leading-[174%] focus:outline-none ml-[22px] mr-[30px]"
+          className="w-[230px] placeholder:text-secondary placeholder:text-base placeholder:leading-[174%] focus:outline-none ml-[22px] mr-[30px]"
         />
         <span className="text-[12px] font-sans text-secondary">
           {inputCount}
