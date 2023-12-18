a943aacdb4942f04bdd670e771d9385c020419df
nayoung3669 <skdud3669@gmail.com>
2023-12-18 18:18:17 +0900

    :recycle: [refactor] : 주석 제거

--- src/app/_service/partner.ts
+++ src/app/_service/partner.ts
@@ -1,6 +1,5 @@
 import api from './axios'
 
-/** 상대방 정보 가져오기 */
 export const getPartnerInfo = async () => {
   try {
     return await api.get('/users/partner', {}).then((res) => res.data)
--- src/app/_types/response.ts
+++ src/app/_types/response.ts
@@ -1,4 +1,3 @@
-/** 채팅 내역 불러오기 응답 데이터 */
 interface ResponseChatGet {
   user: {
     gender: 'MALE' | 'FEMALE'
@@ -14,18 +13,15 @@ interface ResponseChatGet {
   chatList: ChatMessageFromServer[]
 }
 
-/** 메세지 받아오기 응답 데이터 */
 interface ResponseMessage {
   message: string
 }
 
-/** 소셜로그인 응답 데이터 */
 interface ResponseLogin {
   accessToken: string
   refreshToken: string
 }
 
-/** 메인페이지 응답 데이터 */
 interface ResponseMain {
   mainPageResponse: {
     userId: number
@@ -35,7 +31,6 @@ interface ResponseMain {
     roomActive: boolean
   }
 }
-/** 마이페이지 응답 데이터 */
 interface ResponseMyPage {
   myPageResponse: {
     userGender: string
