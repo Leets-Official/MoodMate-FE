3af02b0604e2348842ecffa467fae7410287d5e3
은지 <81403265+eunji0714@users.noreply.github.com>
2024-04-13 20:53:53 +0900

    refactor: redirect to other page after login successfully

--- src/app/(route)/login/kakao/page.tsx
+++ src/app/(route)/login/kakao/page.tsx
@@ -4,9 +4,13 @@ import { useEffect } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
+import { useMainQuery } from '@/_hooks/useMainQuery'
+import Loading from '@/_components/common/Loading'
+import Error from '@/(route)/error'
 
 const LoginHandler = () => {
   const router = useRouter()
+  const { isLoading, isError, data } = useMainQuery()
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -39,7 +43,26 @@ const LoginHandler = () => {
       }
     }
   }, [router])
-
+  if (isLoading) {
+    return <Loading />
+  }
+  if (isError || !data) {
+    return <Error />
+  }
+  useEffect(() => {
+    if (data) {
+      if (
+        data.mainPageResponse.userGender === 'MALE' ||
+        data.mainPageResponse.userGender === 'FEMALE'
+      ) {
+        router.push('/userinfo/1')
+      } else {
+        router.push('/main')
+      }
+    }
+  }, [data, router])
 }
+
+
 
 export default LoginHandler
