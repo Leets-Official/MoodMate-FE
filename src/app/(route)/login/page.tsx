6dacfb786796347d879ef1680f4283cc9819d6d6
은지 <81403265+eunji0714@users.noreply.github.com>
2024-05-12 21:57:44 +0900

    refactor: change text color in login page

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -13,14 +13,14 @@ export default function Login() {
   }
   return (
     <section className="flex flex-col h-screen mx-5 scrollbar-hide">
-      <div className="h-[20%]">
+      <div className="h-[22%]">
         <div className="flex font-bold text-[20px] ml-5 mt-[20%] desktop:mt-[20%]">
           {LOGIN_PAGE.FIRST}
-          <div className="ml-1.5 text-primary">{LOGIN_PAGE.SECOND}</div>
+          <div className="ml-1.5 text-[#E87775]">{LOGIN_PAGE.SECOND}</div>
           {LOGIN_PAGE.THIRD}
         </div>
         <div className="flex font-bold text-[20px] ml-5">
-          <div className="text-primary">{LOGIN_PAGE.FOUR}</div>
+          <div className="text-[#E87775]">{LOGIN_PAGE.FOUR}</div>
           {LOGIN_PAGE.FIVE}
         </div>
       </div>
@@ -45,7 +45,7 @@ export default function Login() {
           <p>카카오계정으로 로그인</p>
         </div>
       </button>
-      <div className="text-center text-xs text-secondary ml-2 mt-4">
+      <div className="text-center text-xs text-gray-400 ml-2 mt-4">
         <p>회원가입 시 개인정보 제공 및 대화 내용 저장에 동의합니다.</p>
         <p>서비스 종료 후 모든 정보는 폐기처리 됩니다.</p>
       </div>
