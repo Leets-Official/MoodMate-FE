a3009db8651bdbea6e4501c15dc084ff477fe655
nayoung3669 <skdud3669@gmail.com>
2024-05-16 01:34:54 +0900

    :recycle: [refactor] : req permission

--- src/app/(route)/main/page.tsx
+++ src/app/(route)/main/page.tsx
@@ -1,19 +1,36 @@
 'use client'
 
 import MainPage from '@/_components/main/MainPage'
-import { useMainQuery } from '@/_hooks/useMainQuery'
 import InactivePage from '@/_components/inactive/InActivePage'
 import Loading from '@/_components/common/Loading'
-import Error from '@/(route)/error'
-import { useEffect } from 'react'
+import ErrorPage from '@/(route)/error'
+import useFirebasePush from '@/_pwa/useFirebasePush'
+import { useEffect, useState } from 'react'
+import { useMainQuery } from '@/_hooks/useMainQuery'
+import { getCookie } from '@/utils/cookieutils'
 
-export default function MainpagePage() {
+export default function MainPagePage() {
   const { isLoading, isError, data } = useMainQuery()
+  const { requestPushPermission } = useFirebasePush()
+
+  const accessToken = getCookie('accessToken')
+  const refreshToken = getCookie('refreshToken')
+
+  useEffect(() => {
+    requestPushPermission()
+  }, [])
+
+  useEffect(() => {
+    if (accessToken?.length === 0 && refreshToken?.length === 0) {
+      window.location.href = '/login'
+    }
+  }, [accessToken, refreshToken])
+
   if (isLoading) {
     return <Loading />
   }
   if (isError || !data) {
-    return <Error />
+    return <ErrorPage />
   }
   const { roomActive, userMatchActive, userGender, roomId, userId } =
     data.mainPageResponse
@@ -21,7 +38,7 @@ export default function MainpagePage() {
   const mainPageGender = userGender === 'MALE' ? 'MALE' : 'FEMALE'
 
   return (
-    <section className="scrollbar-hide">
+    <section className="h-full w-full scrollbar-hide">
       {userMatchActive ? (
         <MainPage type={mainPageType} gender={mainPageGender} />
       ) : (
