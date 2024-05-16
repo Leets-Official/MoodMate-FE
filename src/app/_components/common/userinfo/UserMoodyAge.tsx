02111322de0555e6aa81bb005f163dee16ae6b66
nayoung3669 <skdud3669@gmail.com>
2024-05-16 20:04:30 +0900

    :ambulance: [hotfix] : error redirect to login (개발용)

--- src/app/_components/common/userinfo/UserMoodyAge.tsx
+++ src/app/_components/common/userinfo/UserMoodyAge.tsx
@@ -77,7 +77,7 @@ export default function UserMoodyage({ pageNum, isEdit }: UserMoodyageProps) {
           </div>
         </div>
       </div>
-      <div className="relative top-[0%] mt-[-50px] mb-[20px] left-[40%] ml-[-50px] w-[149px] h-[157px]flex justify-center items-center">
+      <div className="relative top-[0%] mt-[-50px] mb-[20px] left-[40%] ml-[-50px] w-[149px] h-[157px] flex justify-center items-center">
         <Image src={moodyCharacter} alt="" width={149} height={190} />
       </div>
       <div className="justify-items-center flex flex-col items-center">
