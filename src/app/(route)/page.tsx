0d9965cf45ca281ea183736590759f8cbdca5740
One-HyeWon <5876675@naver.com>
2024-05-19 17:24:30 +0900

    :ambulance:[hotfix]: Token 유효성 검사 로직 순서 변경

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,13 +1,21 @@
 'use client'
 
 import Loading from '@/_components/common/Loading'
+import { getCookie } from '@/utils/cookieutils'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function Root() {
   const route = useRouter()
   useEffect(() => {
-    route.push('/main')
+    const accessToken = getCookie('accessToken')
+    const refreshToken = getCookie('refreshToken')
+
+    if ((accessToken || refreshToken) !== undefined) {
+      route.push('/main')
+    } else {
+      route.push('/login')
+    }
   }, [route])
   return (
     <main>
