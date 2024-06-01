e3c53bae67af912cfa5c32cf9d52e6c343771f2e
nayoung3669 <skdud3669@gmail.com>
2024-06-01 20:28:40 +0900

    :ambulance: [hotfix] : 서비스 종료 알림

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,25 +1,10 @@
 'use client'
 
-import Loading from '@/_components/common/Loading'
-import { getCookie } from '@/utils/cookieutils'
-import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function Root() {
-  const route = useRouter()
   useEffect(() => {
-    const accessToken = getCookie('accessToken')
-    const refreshToken = getCookie('refreshToken')
-
-    if ((accessToken || refreshToken) !== undefined) {
-      route.push('/main')
-    } else {
-      route.push('/login')
-    }
-  }, [route])
-  return (
-    <main>
-      <Loading />
-    </main>
-  )
+    throw Error // 서비스 종료
+  }, [])
+  return <main></main>
 }
