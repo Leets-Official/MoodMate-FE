681920af7bbf95ef6dfaeadc91ad17e30e83e659
nayoung3669 <skdud3669@gmail.com>
2024-06-16 16:51:22 +0900

    :ambulance: [hotfix] : 로그인 변경

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,10 +1,27 @@
 'use client'
 
+import Loading from '@/_components/common/Loading'
+
 import { useEffect } from 'react'
+import { getCookie } from '@/utils/cookieutils'
+import { useRouter } from 'next/navigation'
 
 export default function Root() {
+  const route = useRouter()
   useEffect(() => {
-    throw Error // 서비스 종료
-  }, [])
-  return <main></main>
+    const accessToken = getCookie('accessToken')
+    const refreshToken = getCookie('refreshToken')
+
+    if ((accessToken || refreshToken) !== undefined) {
+      route.push('/main')
+    } else {
+      route.push('/login')
+    }
+    // throw Error 서비스 종료 시 코드
+  }, [route])
+  return (
+    <main>
+      <Loading />
+    </main>
+  )
 }
